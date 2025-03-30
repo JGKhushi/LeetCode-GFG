f108a8a11190c33b812d64d50f8b1770d15e19fb
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-03-30 06:34:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node addOne(Node head) {
+        // Reverse the linked list
+        head = reverse(head);
+        
+        Node curr = head;
+        int carry = 1;
+        
+        while (curr != null) {
+            curr.data += carry;
+            if (curr.data < 10) {
+                carry = 0; // No carry to propagate
+                break;
+            } else {
+                curr.data = 0; // Reset to 0 and propagate carry
+                if (curr.next == null) {
+                    curr.next = new Node(1); // Add new node for carry
+                    carry = 0;
+                }
+            }
+            curr = curr.next;
+        }
+        
+        // Reverse back to restore original order
+        return reverse(head);
+    }
+
+    // Helper function to reverse a linked list
+    private Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
