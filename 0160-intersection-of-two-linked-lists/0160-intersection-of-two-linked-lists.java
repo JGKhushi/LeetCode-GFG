bc70dc715190f95039181c8f55bc56f7ad118b7a
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-03-31 12:55:43 +0530

    Time: 595 ms (5.06%), Space: 49 MB (28.53%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,47 +10,48 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
-        // int count = 0 ;
-        // ListNode ans = new ListNode();
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int count = 0 ;
+        ListNode ans = new ListNode();
        
 
-        // ListNode A = new ListNode();
-        // A = headA;
-        // ListNode B = new ListNode();
-        // B= headB;
-        // while(headA != null){
-        //     headB = B;
-        //     while(headB != null){
-        //         if(headA.val == headB.val){
-        //             ans = headA;
-        //             while((headA.val == headB.val) && headA.next != null && headB.next != null){
+        ListNode A = new ListNode();
+        A = headA;
+        ListNode B = new ListNode();
+        B= headB;
+        while(headA != null){
+            headB = B;
+            while(headB != null){
+                if(headA == headB){
+                    ans = headA;
+                    while((headA == headB) && headA.next != null && headB.next != null){
                         
-        //                     headA = headA.next;
-        //                     headB = headB.next;
+                            headA = headA.next;
+                            headB = headB.next;
                         
-        //             }
-        //             if(headA.next == null && headB.next == null){
-        //                 return ans;
-        //             }
-        //         }
-        //         headB = headB.next;
-        //     }
-        //     headA = headA.next;
-        // }
-
-        // return null;
-
-     while(head2 != null) {
-        ListNode temp = head1;
-        while(temp != null) {
-            //if both nodes are same
-            if(temp == head2) return head2;
-            temp = temp.next;
+                    }
+                    if(headA.next == null && headB.next == null){
+                        return ans;
+                    }
+                }
+                headB = headB.next;
+            }
+            headA = headA.next;
         }
-        head2 = head2.next;
-    }
-    //intersection is not present between the lists return null
-    return null;
+
+        return null;
+
+//     public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+    //  while(head2 != null) {
+    //     ListNode temp = head1;
+    //     while(temp != null) {
+    //         //if both nodes are same
+    //         if(temp == head2) return head2;
+    //         temp = temp.next;
+    //     }
+    //     head2 = head2.next;
+    // }
+    // //intersection is not present between the lists return null
+    // return null;
     }
 }
