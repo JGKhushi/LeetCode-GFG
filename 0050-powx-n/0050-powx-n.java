aadb2da215b0d7d4b759d9f1f862a124f9566732
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-04-09 12:04:52 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.05%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n; // Convert to long to avoid overflow for Integer.MIN_VALUE
-
-        if (N == 0) {
-            return 1;
-        }
-
-        double temp = myPow(x, (int)(N / 2));  // store the half power
+        long N = n; // Convert to long to handle Integer.MIN_VALUE
 
         if (N < 0) {
             x = 1 / x;
-            N = -N; // make n positive
-            temp = myPow(x, (int)(N / 2));
+            N = -N;
         }
 
-        if (N % 2 == 0) {
+        return power(x, N);
+    }
+
+    public double power(double x, long n) {
+        if (n == 0) return 1;
+
+        double temp = power(x, n / 2);
+        if (n % 2 == 0) {
             return temp * temp;
         } else {
             return temp * temp * x;
