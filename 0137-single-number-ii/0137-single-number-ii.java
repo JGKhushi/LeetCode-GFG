b7c13c988b552df897caf6208208e646d70ad252
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-04-25 16:21:08 +0530

    Time: 3 ms (65.47%), Space: 45.8 MB (29.91%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,12 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0, twos = 0;
-        
-        for (int num : nums) {
-            ones = (ones ^ num) & ~twos;
-            twos = (twos ^ num) & ~ones;
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+
+            for (int num : nums) {
+                if (((num >> i) & 1) == 1) {
+                    count++;
+                }
+            }
+
+            if (count % 3 != 0) {
+                result |= (1 << i);
+            }
         }
-        
-        return ones;
+
+        return result;
     }
 }
