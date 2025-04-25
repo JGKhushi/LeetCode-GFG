1f65004584f4f580035cc1ba7b80b52b09821196
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-04-25 16:22:11 +0530

    Time: 4 ms (56.08%), Space: 45.6 MB (41.29%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,21 +1,16 @@
+import java.util.Arrays;
+
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
+        Arrays.sort(nums);
+        int n = nums.length;
 
-        for (int i = 0; i < 32; i++) {
-            int count = 0;
-
-            for (int num : nums) {
-                if (((num >> i) & 1) == 1) {
-                    count++;
-                }
-            }
-
-            if (count % 3 != 0) {
-                result |= (1 << i);
+        for (int i = 0; i < n - 2; i += 3) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
             }
         }
 
-        return result;
+        return nums[n - 1]; // Single number is at the end
     }
 }
