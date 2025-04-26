70854609b4efbfc5970bec64c3a19f8f00687046
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-04-26 15:07:07 +0530

    Time: 108 ms (35.63%), Space: 49.2 MB (59.78%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 2 ; i*i<=n ; i++ ){
            
             if(prime[i] == true){
-            for(int j= i*i ; j <= n ; j+=i){
+            for(int j= i*i ; j < n ; j+=i){
                prime[j] = false; 
             }
             }
