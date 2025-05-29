b1a8491dc209d0015c81850dd8044c91b771765c
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-05-29 12:52:35 +0530

    Time: 23 ms (92.31%), Space: 46.6 MB (55.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,24 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int pref = 0;
+        int ans = 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // prefix sum = 0 occurs once initially
 
-    int n = nums.length;
-    int[] pref = new int[n];
-    pref[0] = nums[0];
-     for(int i = 1; i< n ; i++ ){
-        pref[i] = pref[i-1] + nums[i]; 
-     }
+        for (int i = 0; i < n; i++) {
+            pref += nums[i];
 
-    int ans = 0;
-    for(int i = 0 ; i< n ; i++){
-        for(int j = i ; j< n ; j++){
-              int curr = (i == 0) ? pref[j] : pref[j] - pref[i - 1];
-            if(curr == k){
-                ans++;
+            if (map.containsKey(pref - k)) {
+                ans += map.get(pref - k);  // add frequency of (pref - k)
             }
+
+            map.put(pref, map.getOrDefault(pref, 0) + 1);  // update prefix frequency
         }
-    }
 
-    return ans;
-
+        return ans;
     }
 }
