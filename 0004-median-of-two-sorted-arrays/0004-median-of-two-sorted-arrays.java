5634cbba3e0719524e6fa5bc6fe27c129f3b0385
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-06-22 16:24:22 +0530

    Time: 1 ms (100%), Space: 46.2 MB (60.8%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,28 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int total = n + m;
-        int mid = total / 2;
+        if(nums1.length>nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int left = 0;
+        int right = len1;
+        while(left <= right) {
+            int partition1 = (left + right)/2;
+            int partition2 = (len1 + len2 + 1)/2 - partition1;
 
-        int idx1 = 0, idx2 = 0;
-        int cnt = 0;
-        int curr = 0, prev = 0;
+            int maxLeft1 = (partition1 == 0) ? Integer.MIN_VALUE : nums1[partition1 - 1];
+            int minRight1 = (partition1 == len1) ? Integer.MAX_VALUE : nums1[partition1];
+            int maxLeft2 = (partition2 == 0) ? Integer.MIN_VALUE : nums2[partition2 - 1];
+            int minRight2 = (partition2 == len2) ? Integer.MAX_VALUE : nums2[partition2];
 
-        while (cnt <= mid) {
-            prev = curr;
-            if (idx1 < n && (idx2 >= m || nums1[idx1] <= nums2[idx2])) {
-                curr = nums1[idx1++];
-            } else {
-                curr = nums2[idx2++];
+            if(maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                if((len1 + len2) %2 == 0) {
+                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) /2.0;
+                } 
+                else {
+                    return (double) Math.max(maxLeft1, maxLeft2);
+                }
             }
-            cnt++;
+            else if(maxLeft1 > minRight2) {
+                right = partition1 - 1;
+            }
+            else {
+                left = partition1 + 1;
+            }
         }
-
-        if (total % 2 == 0) {
-            return (prev + curr) / 2.0;
-        } else {
-            return curr;
-        }
+        return 0.0;
     }
 }
