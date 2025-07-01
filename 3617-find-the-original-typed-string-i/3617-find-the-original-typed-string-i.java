280c38cbef344fef0945a4706a5b7dba4846b2f2
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-07-01 06:52:07 +0530

    Time: 1 ms (87.19%), Space: 42.3 MB (38.84%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -2,30 +2,16 @@ class Solution {
     public int possibleStringCount(String word) {
         int n= word.length();
         
-        int ans = 0 ;
         int cnt = 1;
         for(int i = 1 ; i< n ; i++){
             if(word.charAt(i) == word.charAt(i-1)){
                 cnt++;
             }
-            else{
-                if(cnt > 1){
-                    ans = ans + cnt - 1;
-                }
-                
-                cnt = 1;
-            }
-
-            if(i == n-1){
-                if(cnt > 1){
-                    ans = ans + cnt - 1;
-                }
-                
-            }
+            
         }
 
        
 
-        return ans+1 ;
+        return cnt ;
     }
 }
