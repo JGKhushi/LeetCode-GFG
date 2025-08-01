f8490e0b294f113bbc9584db58e0a5026a9b152e
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-08-01 20:09:15 +0530

    Time: 0 ms (100%), Space: 40.7 MB (38.39%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -1,30 +1,36 @@
 class Solution {
-    int mod = 1000000007;
 
-    public int countGoodNumbers(long n) {
-       
+     static final int MOD = 1_000_000_007; 
+     
+    public static  long pow(int n , long x){
+        if(x == 0 ){
+            return 1 ;
+        }
+    long half = pow(n , x/2);
 
-        long evenWays = fastPower(20, n/2);
-       
-
-if(n%2 == 1){
-    evenWays*=5;
-}
-        return (int)((evenWays) % mod);
+        if(x % 2 == 0){
+            return (half * half)% MOD ; 
+        }
+        return( half * half * n)% MOD;
     }
-
-    private long fastPower(long base, long power) {
-        long result = 1;
-   
-
-        while (power > 0) {
-            if (power % 2 == 1) {
-                result = (result * base) % mod;
-            }
-            base = (base * base) % mod;
-            power /= 2;
+    public int countGoodNumbers(long n) {
+        long even = 0 ;
+        long odd = 0 ;
+        if( n % 2 ==  0){
+            // even places 
+        even =  pow(5 , n/2 );
+        // odd places 
+         odd = pow(4 , n/2 );
+        }
+        else{
+            // even places 
+        even =  pow(5 , (n/2)+1 );
+        // odd places 
+         odd = pow(4 , n/2 );
         }
 
-        return result;
+        long result = (even * odd) % MOD;
+return (int) result;
+
     }
 }
