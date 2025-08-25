44412f8478165ef0a3b9661136312a34b42bf7d8
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-08-25 14:24:29 +0530

    Time: 2 ms (99.41%), Space: 45.7 MB (97.58%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,31 +1,26 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int n = mat.length;        // number of rows
-        int m = mat[0].length;     // number of cols
-        int ans[] = new int[n*m];  // total elements
-        int k = 0;
+        int n= mat.length;
+        int m = mat[0].length;
+        int[] ans = new int[n*m];
+        int k = 0 ;
 
-        // total diagonals = n+m-1
-        for (int i = 0; i < n + m - 1; i++) {
-            int a, b;
-
-            if (i % 2 == 0) {   // even diagonal → up direction
-                a = (i < n) ? i : n - 1;
-                b = i - a;
-                while (a >= 0 && b < m) {
-                    ans[k] = mat[a][b];
-                    // System.out.println(a + " " + b);
-                    k++;
+        for(int i = 0 ; i< n+m-1 ; i++ ){
+            int a , b ;
+            if(i%2 == 0){
+                a = (i<n)?i:n-1;
+                b = i-a ;
+                while( a>= 0 && b < m){
+                    ans[k++] = mat[a][b];
                     a--;
                     b++;
                 }
-            } else {           // odd diagonal → down direction
-                b = (i < m) ? i : m - 1;
-                a = i - b;
-                while (a < n && b >= 0) {
-                    ans[k] = mat[a][b];
-                    // System.out.println(a + " " + b);
-                    k++;
+            }
+            else{
+                b =  (i<m)?i : m-1;
+                a = i-b;
+               while (a < n && b >= 0) {
+                    ans[k++] = mat[a][b];
                     a++;
                     b--;
                 }
