235fc94ecd9934d4b5e28e7aa486faa2ec418452
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-08-31 15:00:00 +0530

    Time: 110 ms (5.38%), Space: 45.2 MB (30.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,42 +1,33 @@
 class Solution {
-
-    public  int fn(int arr[] , int n , int T , int[][] dp){
-        if(n == 0){
-        if(T % arr[0] == 0){
-            return T/arr[0];
-        }
-        else{
-            return (int)Math.pow(10 , 9);
-        }
-        }
-
-
-        if(dp[n][T] != -1){
-            return  dp[n][T];
-        }
-
-        int notTake = fn(arr , n-1 , T , dp);
-        int take =(int) Math.pow(10,9);
-        if(arr[n] <= T){
-            take = 1 + fn(arr , n , T -arr[n] ,dp);
-        }
-
-        return dp[n][T] = Math.min(notTake ,take);
-
-
-    }
     public int coinChange(int[] arr, int T) {
         int n = arr.length;
         int dp[][] = new int[n][T+1];
-        for(int rows[] : dp){
-            Arrays.fill(rows , -1);
+        for(int i = 0 ; i<= T ; i++){
+            if(i % arr[0] == 0){
+                dp[0][i] = i / arr[0];
+            }
+            else{
+                dp[0][i] = (int)Math.pow(10,9);
+            }
         }
-        int ans = fn(arr , n - 1 , T , dp);
 
-        if(ans >=  (int) Math.pow(10,9)) 
-        return -1 ;
-        
+        for(int ind = 1 ; ind< n ; ind++){
+            for(int target = 0 ; target<= T ; target++){
+                int notTake = 0 + dp[ind - 1][target];
+                int take = (int) Math.pow(10 , 9);
+                if(arr[ind] <= target){
+                    take = 1+ dp[ind][target - arr[ind]] ;
+                }
+
+                dp[ind][target] = Math.min(take , notTake);
+            }
+        }
+
+        int ans = dp[n-1][T];
+        if(ans >= Math.pow(10,9)){
+            return -1;
+        }
+
         return ans;
-
     }
 }
