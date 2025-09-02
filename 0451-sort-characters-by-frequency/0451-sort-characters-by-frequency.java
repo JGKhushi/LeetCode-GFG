9a7fb211db7ca710fdc0fa73b0313fea0337ba3a
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-02 12:14:50 +0530

    Time: 378 ms (7.36%), Space: 46.3 MB (9.74%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,34 +1,37 @@
 class Solution {
     public String frequencySort(String s) {
-        int n = s.length();
-        int[] arr = new int[128];
-        int[] arr2 = new int[128];
+        int[] arr1  = new int[76];
+        int[] arr2  = new int[76];
 
-        for(int i = 0 ; i < n ; i++){
-            arr[s.charAt(i)]++;
-            arr2[s.charAt(i)]++;
+        int n = s.length();
+
+        for(int i = 0 ; i< n ; i++){
+            int c = s.charAt(i)-'0';
+            arr1[c]++;
+            arr2[c]++;
         }
 
-        Arrays.sort(arr);
-
         String ans = "";
-
-        for(int i = 127 ; i >= 0 ; i--){
-            if(arr[i] != 0){
-                for(int j = 0 ; j < 128 ; j++){
-                    if(arr[i] == arr2[j]){
-                        int cnt = arr[i];
-                        while(cnt > 0){
-                            ans += (char)(j);
-                            cnt--;
-                        }
-                        arr2[j] = 0;
-                        break;
+        Arrays.sort(arr1);
+        for(int i = 75 ; i>= 0 ; i--){
+            if(arr1[i] == 0){
+                continue;
+            }
+            for(int j = 0 ; j <= 75 ; j++){
+                if(arr1[i] == arr2[j]){
+                    int count = arr1[i] ;
+                    while(count-- > 0){
+                        ans+= (char)('0'+j) ;
                     }
+                    arr2[j] = 0;
+                    break;
                 }
             }
         }
 
+
         return ans;
+
+
     }
 }
