1d9a89b51c5ea96054af6a2afd68f1172c30c3d3
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-02 12:42:21 +0530

    Time: 377 ms (7.59%), Space: 45.7 MB (66.04%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,37 +1,26 @@
 class Solution {
     public String frequencySort(String s) {
-        int[] arr1  = new int[76];
-        int[] arr2  = new int[76];
+        Map<Character,Integer> hmap = new HashMap<>();
 
-        int n = s.length();
+        int n= s.length();
 
         for(int i = 0 ; i< n ; i++){
-            int c = s.charAt(i)-'0';
-            arr1[c]++;
-            arr2[c]++;
+            hmap.put(s.charAt(i) , hmap.getOrDefault(s.charAt(i) , 0)+ 1);
         }
 
+        List<Map.Entry<Character,Integer>> list = new ArrayList<>(hmap.entrySet());
+        list.sort((a,b)->b.getValue() - a.getValue());
+ 
         String ans = "";
-        Arrays.sort(arr1);
-        for(int i = 75 ; i>= 0 ; i--){
-            if(arr1[i] == 0){
-                continue;
-            }
-            for(int j = 0 ; j <= 75 ; j++){
-                if(arr1[i] == arr2[j]){
-                    int count = arr1[i] ;
-                    while(count-- > 0){
-                        ans+= (char)('0'+j) ;
-                    }
-                    arr2[j] = 0;
-                    break;
-                }
+        for(Map.Entry<Character, Integer> e :list){
+            int v = e.getValue();
+            char k = e.getKey();
+
+            while(v-- > 0){
+                ans+=k ;
             }
         }
-
 
         return ans;
-
-
     }
 }
