f783fe7912aad7d5a925b31d47897dedb6584009
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-02 11:16:39 +0530

    Time: 0 ms (100%), Space: 41.6 MB (53.19%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String g) {
+        if(s.length() != g.length()){
+            return false ;
+        }
+        String ns = s + s;
+        if(ns.contains(g)){
+            return true ;
+        }
+
+        return false;
+    }
+}
