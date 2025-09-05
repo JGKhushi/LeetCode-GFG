6f29e83efff505444f71186cd2df97eb45b95da6
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-05 05:51:34 +0530

    Time: 629 ms (72.78%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,12 +1,1 @@
-SELECT 
-    s.user_id,
-    ROUND(
-        CASE 
-            WHEN COUNT(c.user_id) = 0 THEN 0
-            ELSE SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) * 1.0 / COUNT(c.user_id)
-        END
-    , 2) AS confirmation_rate
-FROM Signups AS s
-LEFT JOIN Confirmations AS c
-    ON s.user_id = c.user_id
-GROUP BY s.user_id;
+SELECT s.user_id, ROUND( AVG(CASE WHEN action = 'confirmed' then 1 else 0 end) , 2 ) as confirmation_rate from Signups s left join Confirmations c on s.user_id = c.user_id GROUP BY user_id ;
