7b5199d72e650155bf5027a2d65568f3d881859c
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-08 18:19:57 +0530

    Time: 11 ms (5.55%), Space: 43.5 MB (34.78%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,18 +1,18 @@
 class Solution {
 
     public static void fn(int n , String s , List<String> ans , int open , int close){
-        if(close > open || open > n || close > n  ){
-            return ;
-        }
-       
+        
         if(s.length() == n*2 && open == close){
             System.out.println(s);
             ans.add(s);
             return;
         }
+        if(open < n){
         fn(n , s+'(' , ans , open+1 , close) ;
+        }
+        if(close < open){
         fn(n , s+')' , ans , open , close+1) ;
-
+        }
     }
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
