ce9a14afb3d18e4ad15f238d88443c28fc65f724
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-12 07:27:33 +0530

    Time: 285 ms (64.89%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-SELECT name, population, area FROM World WHERE (population >= 25000000 || area >= 3000000  );
+SELECT name, population, area 
+FROM World 
+WHERE (population >= 25000000 or area >= 3000000  );
