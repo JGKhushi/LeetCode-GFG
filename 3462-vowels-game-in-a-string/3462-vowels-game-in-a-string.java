e9c569d0d5a1473fe6606f94013c9b8d540b440f
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-12 11:05:58 +0530

    Time: 14 ms (25.72%), Space: 46 MB (8.66%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+     int vowel_count = 0 ;
+
+     for(int i = 0 ; i< s.length() ; i++){
+        if(s.charAt(i) == 'a' ||s.charAt(i) == 'e' ||s.charAt(i) == 'i' ||s.charAt(i) == 'o' || s.charAt(i) == 'u'   ){
+            vowel_count++;
+        }
+     }
+     if(vowel_count == 0 ){
+        return false;
+     }
+    return true;
+    
+    }
+}
