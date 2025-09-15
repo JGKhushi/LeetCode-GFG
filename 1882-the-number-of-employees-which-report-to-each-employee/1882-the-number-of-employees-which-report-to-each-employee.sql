e4bc1974c7a6836963f9db7a85f716a90969dd0c
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-15 08:14:58 +0530

    Time: 601 ms (77.07%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,9 +1,20 @@
 # Write your MySQL query statement below
 /* Write your PL/SQL query statement below */
-SELECT E1.employee_id, E1.name, COUNT(E2.employee_id)  reports_count,
+-- SELECT E1.employee_id, E1.name, COUNT(E2.employee_id)  reports_count,
 
-ROUND(AVG(E2.age)) average_age FROM Employees E1 INNER JOIN Employees 
+-- ROUND(AVG(E2.age)) average_age FROM Employees E1 INNER JOIN Employees 
 
-E2 ON E1.employee_id = E2.reports_to GROUP BY E1.employee_id, E1.name 
+-- E2 ON E1.employee_id = E2.reports_to GROUP BY E1.employee_id, E1.name 
 
-ORDER BY E1.employee_id
+-- ORDER BY E1.employee_id
+
+SELECT E1.employee_id,
+       E1.name,
+       COUNT(E2.employee_id)        AS reports_count,
+       ROUND(AVG(E2.age))          AS average_age
+FROM Employees E1
+LEFT JOIN Employees E2
+  ON E2.reports_to = E1.employee_id
+GROUP BY E1.employee_id, E1.name
+HAVING COUNT(E2.employee_id) > 0
+ORDER BY E1.employee_id;
