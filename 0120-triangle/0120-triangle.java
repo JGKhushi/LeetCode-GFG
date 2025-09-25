6e4211df1a4efdfa88d8393caa248390a816998a
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-25 12:03:12 +0530

    Time: 3 ms (79.02%), Space: 45.7 MB (20.7%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,26 +1,13 @@
 class Solution {
 
-    public static int fn(int i , int j , List<List<Integer>> triangle , int[][] dp){
-        int n = triangle.size();
-        if(i == n-1){
-            return triangle.get(i).get(j);
-        }
-        if(dp[i][j] != Integer.MAX_VALUE){return dp[i][j];}
-
-        int down = fn(i+1 , j , triangle , dp);
-        int diag = fn(i+1 , j+1 , triangle , dp);
-
-        dp[i][j] = triangle.get(i).get(j) + Math.min(down , diag);
-        return dp[i][j];
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
      int n = triangle.size();
-     int dp[][] = new int[n][n]; 
-     for(int i =  0 ; i< n ; i++){
-        for(int j = 0 ; j< n ; j++){
-            dp[i][j] = Integer.MAX_VALUE;
+     int dp[] = new int[n+1]; 
+     for(int i =  n-1 ; i>=0 ; i--){
+        for(int j = 0 ; j<=i ; j++){
+            dp[j] = Math.min(dp[j] , dp[j+1]) + triangle.get(i).get(j); 
         }
      } 
-     return fn(0,0,triangle,dp); 
+     return dp[0]; 
     }
 }
