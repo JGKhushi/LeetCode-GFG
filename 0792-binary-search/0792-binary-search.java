0d43aad432bcae5b005a935e815fa7c2b9b3e776
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-09-28 07:35:39 +0530

    Time: 0 ms (100%), Space: 46 MB (34.92%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,22 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int  n = nums.length;
-        int low = 0 ;
-         int high = n - 1 ;
-        
-        while(low <= high){
-            int mid =( low + high )/ 2 ;
-            if(nums[mid] == target ){
-                return mid;
-            }
-            else if(nums[mid ] < target){
+         int n = nums.length ;
+         int high = n -1 ;
+         int low = 0 ;
+         while(low <= high){
+            int mid = low + (high - low)/2 ;
+            if(target > nums[mid]){
                 low = mid + 1 ;
+            }else if(target < nums[mid]){
+                high = mid - 1 ;
+            }else{
+                return mid ;
             }
-            else{
-                high = mid - 1;
-            }
-        }
+         }
 
-        return -1;
+         return - 1;
     }
 }
