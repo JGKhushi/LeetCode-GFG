5ea5d194048d88b6e1e6d7bc29166c99f0246197
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-01 16:08:38 +0530

    Time: 0 ms (100%), Space: 40.8 MB (97.15%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,41 +1,31 @@
 class Solution {
-
-    public static int solve(int[] nums){
-       int n = nums.length;
-       int prev = nums[0];
-       int prev2 = 0 ;
-
-        for(int i = 1 ; i< n ;i++){
-            int pick = nums[i];
-            if(i>1){
-                pick += prev2;
-            }
-                int nonpick = 0+ prev;
-                int cur_i = Math.max(pick , nonpick);
-                prev2 = prev ;
-                prev = cur_i;
-        } 
-
-        return prev;
+    public int fn(int[] nums ,int start , int end , int dp[] ){
+             Arrays.fill(dp, 0);
+             dp[start] = nums[start];
+        if (start + 1 <= end) {
+            dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        }
+             for(int i = 2 ; i<= end ; i++){
+                int notp = dp[i-1];
+                int pick = nums[i] + dp[i-2];
+                dp[i] = Math.max(pick , notp);
+        }
+        return dp[end];
     }
     public int rob(int[] nums) {
         int n = nums.length;
-     int arr1[] = new int[n];
-     int arr2[] = new int[n];
+        int dp[] = new int[n];
+        
+         if(n==1){
+            return nums[0];
+        }
+        if(n == 2){
+            return Math.max(nums[0] , nums[1]);
+        }
 
-     if(n==1){
-        return nums[0];
-     }
+        int ans1 = fn(nums ,0 , n-2 , dp );
+        int ans2 = fn(nums ,1 , n-1 , dp);
 
-     for(int i = 0 ; i< n ; i++){
-        if(i != 0 ) arr1[i]  = nums[i];
-        if(i != n-1 ) arr2[i]  = nums[i];
-     }
-
-     int ans1 = solve(arr1);
-     int ans2 = solve(arr2);
-
-
-     return Math.max(ans1 , ans2);
+        return Math.max(ans1 , ans2 );
     }
 }
