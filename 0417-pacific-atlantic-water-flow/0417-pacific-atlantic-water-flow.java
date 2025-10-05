0ba3a8387578cc9ff556be8316ff1ec5625173d1
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-05 19:22:02 +0530

    Time: 4 ms (97.67%), Space: 46.4 MB (6.34%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -0,0 +1,38 @@
+class Solution {
+
+private int m , n ;
+private int[][] directions = {{1,0} , {-1 , 0 } , {0,1} , {0,-1}} ;
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        m = heights.length; //rows 
+        n = heights[0].length; // cols
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for(int j = 0 ; j<n ; j++)dfs(0,j,heights, pacific ) ;
+        for(int i = 0 ; i<m ; i++)dfs(i,0,heights,pacific);
+        for(int j = 0 ; j<n ; j++)dfs(m-1,j,heights, atlantic ) ;
+        for(int i = 0 ; i<m ; i++)dfs(i,n-1,heights,atlantic);
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0 ; j< n ; j++){
+                if(pacific[i][j] && atlantic[i][j]) result.add(Arrays.asList(i,j));
+            }
+        }
+        return result;
+    }
+
+    private void dfs(int i , int j , int[][] heights , boolean[][] visited){
+        if(visited[i][j]) return ;
+        visited[i][j] = true;
+        for(int[] d : directions){
+            int x = i+ d[0] , y = j +d[1];
+            if(x <  0 || x >= m || y<0 || y>=n ) continue;
+            if(heights[i][j] > heights[x][y])continue;
+
+            dfs(x , y, heights , visited);
+        }
+    }
+}
