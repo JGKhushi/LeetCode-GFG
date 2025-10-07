82ffe1fdcbe113469eac3d030992d8fbba4d6edf
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-07 10:28:06 +0530

    Time: 1 ms (99.87%), Space: 48.8 MB (45.84%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -17,7 +17,7 @@ public class Solution {
         while(l1 != l2){
             if(l1 == null){l1 = headB; cnt++;}else{l1 = l1.next;}
             if(l2 == null){l2 = headA; cnt++;}else{l2 = l2.next;}
-            if(cnt >= 3)return null;
+            // if(cnt >= 3)return null;
         }    
 
         return l1;
