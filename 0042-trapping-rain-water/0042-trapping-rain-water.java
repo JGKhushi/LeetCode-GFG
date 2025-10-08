7f9eff4c7fbdd3f31444e62dac4ae7c6264b93d5
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-08 10:43:41 +0530

    Time: 0 ms (100%), Space: 46.9 MB (9.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length ;
-        int res = 0  , maxLeft = 0 , maxRight = 0 , left = 0 , right = n- 1;
-        while(left <= right){
+        int ans = 0 ;
+        int n = height.length;
+        int maxLeft = 0 , maxRight = 0 , left = 0 , right = n-1 ;
+        while(left <= right ){
             if(height[left] <= height[right]){
                 if(height[left] >= maxLeft){
-                    maxLeft =  height[left];
-                }
-                else{
-                    res += maxLeft - height[left];
+                    maxLeft = height[left];
+                }else{
+                    ans  += maxLeft - height[left];
                 }
                 left++;
-            }else{
-                if(height[right] >= maxRight){
+            }
+            else{
+                if(maxRight <= height[right]){
                     maxRight = height[right];
-                }
-                else{
-                    res += maxRight - height[right];
+                }else{
+                    ans += maxRight - height[right];
                 }
                 right--;
             }
         }
 
-        return res;
+        return ans;
     }
 }
