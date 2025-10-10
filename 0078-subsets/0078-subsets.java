ea34a6bf96c5a92439f5fc6b2658e46f9f389d35
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-10 13:35:06 +0530

    Time: 1 ms (48.08%), Space: 43 MB (18.5%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,23 +1,17 @@
-import java.util.*;
-
 class Solution {
-    public static void fn(int[] nums, List<List<Integer>> lst, List<Integer> al, int i) {
-        // add current subset (copy of al)
-        lst.add(new ArrayList<>(al));
-
-        // explore further elements
-        for (int j = i; j < nums.length; j++) {
-            // include nums[j]
-            al.add(nums[j]);
-            fn(nums, lst, al, j + 1);
-            // backtrack (remove last element)
-            al.remove(al.size() - 1);
-        }
-    }
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> lst = new ArrayList<>();
-        fn(nums, lst, new ArrayList<>(), 0);
-        return lst;
+        List<List<Integer>> output = new ArrayList<>();
+        int n = nums.length ;
+        for(int i = (int) Math.pow(2,n) ; i< (int)Math.pow(2,n+1) ; ++i){
+            String bitMask = Integer.toBinaryString(i).substring(1);
+            List<Integer> curr = new ArrayList();
+
+            for(int j = 0 ; j< n ; ++j){
+                if(bitMask.charAt(j) == '1')curr.add(nums[j]);
+            }
+
+            output.add(curr);
+        }
+        return output;
     }
 }
