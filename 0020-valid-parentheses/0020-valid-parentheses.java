e58c11e744df2ef3c89b0e36845be76785c29839
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-11 15:03:26 +0530

    Time: 3 ms (54.75%), Space: 42.3 MB (13.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
         int n = s.length();
-        Stack<Character> stk= new Stack<>();
-        for(int i=0;i<n;i++){
-            //  if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
-            //     stk.push(s.charAt(i));
-            //  }
-             if(!stk.isEmpty() && ((s.charAt(i) == ')' && stk.peek() == '(') || (s.charAt(i) == ']' && stk.peek() == '[') || (s.charAt(i) == '}' && stk.peek() == '{'))){
-                stk.pop();
-             }
-             else{
+        for(int i = 0 ; i<n ; i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'  ){
                 stk.push(s.charAt(i));
-             }
-
-
+            }
+            else{
+                if(!stk.isEmpty() && ( (s.charAt(i) == ')' && stk.peek() == '(') || (s.charAt(i) == ']' && stk.peek() == '[') ||  (s.charAt(i) == '}' && stk.peek() == '{'))){
+                    stk.pop();
+                }else{
+                    return false;
+                }
+            }
         }
 
-        if(stk.isEmpty()){
-            return true;
-        }
-       
-       return false;
+        if(stk.isEmpty())return true;
+
+        return false;
     }
 }
