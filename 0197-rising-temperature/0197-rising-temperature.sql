40e18e20aa7ce3a166042b227615a86e3e510305
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-14 06:47:29 +0530

    Time: 479 ms (51.8%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT w2.id FROM Weather AS w1, Weather AS w2 WHERE DATEDIFF(w2.recordDate , w1.recordDate)= 1 AND w1.temperature < w2.temperature;
+SELECT w2.id FROM Weather AS w1 CROSS JOIN Weather as w2 WHERE DATEDIFF(w2.recordDate ,w1.recordDate) = 1 AND w1.temperature < w2.temperature;
