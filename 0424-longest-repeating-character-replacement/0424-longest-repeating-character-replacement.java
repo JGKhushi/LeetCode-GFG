f82f661ff210dec78e532df1112b5a3987cb97e2
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-16 17:50:47 +0530

    Time: 26 ms (29.21%), Space: 45.2 MB (24.7%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,24 +1,20 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int n = s.length();
-        int res = 0 ;
-        int j = 0;
-        HashMap<Character , Integer> hmap = new HashMap<>();
-        int maxFreq = 0 ; 
-
-        for(int i=0; i<n ; i++){
-           hmap.put(s.charAt(i) , hmap.getOrDefault(s.charAt(i) , 0) + 1); 
-            maxFreq = Math.max(maxFreq , hmap.get(s.charAt(i)));
-
-            while((i - j + 1 ) - maxFreq > k ){
-                hmap.put(s.charAt(j) , hmap.getOrDefault(s.charAt(j) , 0) - 1); 
-                j++;
+        int ans = 0 ; 
+        int start = 0 ;
+        int maxFreq =  0;
+        HashMap<Character,Integer> hmap = new HashMap<>();
+        for(int  i = 0 ; i<n ; i++){
+             hmap.put(s.charAt(i) , hmap.getOrDefault(s.charAt(i) , 0) + 1);
+          maxFreq = Math.max(  hmap.get(s.charAt(i)) , maxFreq) ;
+            while(i-start+1 - maxFreq  > k ){
+                char c =s.charAt(start);
+                hmap.put(c , hmap.get(c) - 1);
+                start++;
             }
-
-            res = Math.max(res , i-j+1);
+            ans = Math.max(ans , i-start+1);
         }
-
-    return res ; 
-
+        return ans ;
     }
 }
