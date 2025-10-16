9630b9a1d238c22f7872da45f14bde8c779e9fbb
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-16 09:00:06 +0530

    Time: 580 ms (42.69%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -8,3 +8,10 @@ FROM (
 LEFT JOIN Activity t2
     ON t1.player_id = t2.player_id
    AND DATEDIFF(t2.event_date, t1.first_login) = 1;
+   
+
+-- | t1.player_id | first_login | t2.player_id | t2.event_date |
+-- | ------------ | ----------- | ------------ | ------------- |
+-- | 1            | 2016-03-01  | 1            | 2016-03-02    |
+-- | 2            | 2017-06-25  | NULL         | NULL          |
+-- | 3            | 2016-03-02  | NULL         | NULL          |
