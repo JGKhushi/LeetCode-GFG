4e8bff217b2488c1f1882170838eed2751684a6c
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-22 07:30:34 +0530

    Time: 12 ms (81.46%), Space: 56.9 MB (83.44%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,63 +1,31 @@
 class Solution {
-
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        Arrays.sort(nums);
-
-        int ans = 0;
-        Map<Integer, Integer> numCount = new HashMap<>();
-
-        int lastNumIndex = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] != nums[lastNumIndex]) {
-                numCount.put(nums[lastNumIndex], i - lastNumIndex);
-                ans = Math.max(ans, i - lastNumIndex);
-                lastNumIndex = i;
-            }
+        int mx = Arrays.stream(nums).max().getAsInt();
+        int n= mx + k + 1;
+        int f[] = new int[n];
+        for(int num : nums)f[num]++ ;
+        int cf[] = new int[n];
+        cf[0]=Math.max(0,f[0]);
+        for(int i = 1 ; i< n ; i++){
+            cf[i]= cf[i-1]+f[i];
         }
 
-        numCount.put(nums[lastNumIndex], nums.length - lastNumIndex);
-        ans = Math.max(ans, nums.length - lastNumIndex);
-
-        for (int i = nums[0]; i <= nums[nums.length - 1]; i++) {
-            int l = leftBound(nums, i - k);
-            int r = rightBound(nums, i + k);
-            int tempAns;
-            if (numCount.containsKey(i)) {
-                tempAns = Math.min(r - l + 1, numCount.get(i) + numOperations);
-            } else {
-                tempAns = Math.min(r - l + 1, numOperations);
+        int ans = 0 ;
+        for(int i =0 ; i<n; i++ ){
+            int l = Math.max(i-k , 0 );
+            int r = Math.min(i + k , n-1);
+            int freq = 0 ;
+            if(l>0)
+             freq = cf[r] - cf[l-1] ; //total frequency
+            else {
+              freq = cf[r] ;
             }
-            ans = Math.max(ans, tempAns);
+            int need = freq - f[i]; //need to be adjusted ;
+            int possible = Math.min(numOperations , need); //possible to adjust 
+            int total =  f[i] + possible ; //total frequency after applying operations
+            ans = Math.max(total , ans);
         }
 
-        return ans;
-    }
-
-    private int leftBound(int[] nums, int value) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] < value) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
-    }
-
-    private int rightBound(int[] nums, int value) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right + 1) / 2;
-            if (nums[mid] > value) {
-                right = mid - 1;
-            } else {
-                left = mid;
-            }
-        }
-        return left;
+        return ans ;
     }
 }
