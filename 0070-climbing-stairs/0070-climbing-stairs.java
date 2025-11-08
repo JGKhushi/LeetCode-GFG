dca9e2614462ff217d6351e4c7b3284f65b1e17f
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-08 17:31:34 +0530

    Time: 0 ms (100%), Space: 42.2 MB (6.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,24 @@
 class Solution {
+    public int fn(int n , int dp[] , int i ){
+        if(i == n){
+            return 1;
+        }
+        if(i > n){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int first = fn(n , dp , i+1);
+        int second = fn(n , dp , i+2);
+        dp[i] = first +second ;
+        return first + second ;
+    }
     public int climbStairs(int n) {
-       int dp[] = new int[n+1];
-
-    dp[0] = 0 ;
-    if(n >= 1){
-    dp[1] = 1 ;
-    }
-    if(n >= 2){
-
-    dp[2] = 2 ;
-    }
-
-      for(int i = 3   ; i<= n ; i++){
-         dp[i] = dp[i-1] + dp[i-2];
-      }
-
-      return dp[n];
+        int dp[] = new int [n+1] ; 
+        Arrays.fill(dp , -1);
+        fn(n , dp , 0 );
+        return dp[0];
     }
 }
