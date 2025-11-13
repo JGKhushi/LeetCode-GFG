7a8cdda2a17fe7ac7a46e257da989b00f769d773
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-13 19:42:27 +0530

    Time: 385 ms (5.06%), Space: 48.1 MB (5.01%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,31 +1,26 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        Map<Character,Integer> hmap = new HashMap<>();
+
+        int n= s.length();
+
+        for(int i = 0 ; i< n ; i++){
+            hmap.put(s.charAt(i) , hmap.getOrDefault(s.charAt(i) , 0)+ 1);
         }
 
-        // Step 2: Make frequency buckets
-        // max frequency can be s.length()
-        List<Character>[] bucket = new ArrayList[s.length() + 1];
-        for (char c : map.keySet()) {
-            int freq = map.get(c);
-            if (bucket[freq] == null) bucket[freq] = new ArrayList<>();
-            bucket[freq].add(c);
-        }
+        List<Map.Entry<Character,Integer>> list = new ArrayList<>(hmap.entrySet());
+        list.sort((a,b)->b.getValue() - a.getValue());
+ 
+        String ans = "";
+        for(Map.Entry<Character, Integer> e :list){
+            int v = e.getValue();
+            char k = e.getKey();
 
-        // Step 3: Build string from highest frequency to lowest
-        StringBuilder sb = new StringBuilder();
-        for (int i = bucket.length - 1; i >= 0; i--) {
-            if (bucket[i] != null) {
-                for (char c : bucket[i]) {
-                    for (int j = 0; j < i; j++) {
-                        sb.append(c);
-                    }
-                }
+            while(v-- > 0){
+                ans+=k ;
             }
         }
 
-        return sb.toString();
+        return ans;
     }
 }
