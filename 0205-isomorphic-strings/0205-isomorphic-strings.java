cbe0fec03ab8783e7b585ea481efbb757a52b877
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-13 18:58:09 +0530

    Time: 16 ms (40.84%), Space: 43.4 MB (18.7%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,40 +1,29 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> hmap1 = new HashMap<>();
-        HashMap<Character, Character> hmap2 = new HashMap<>();
+        HashMap<Character , Character > hmap1 = new HashMap<>();
+        HashMap<Character , Character > hmap2 = new HashMap<>();
+        int sl = s.length() ; 
+        int tl = t.length() ;
+        if(sl != tl) return false;
+        for(int i = 0 ; i< sl ; i++){
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+           if(hmap1.containsKey(sc) ){
+            if( hmap1.get(sc) != tc )return false ;
+           }
+           else{
+            hmap1.put(sc , tc);
+           }
 
-       int n = s.length();
-       int m = t.length();
+            if(hmap2.containsKey(tc) ){
+            if( hmap2.get(tc) != sc )return false ;
+           }
+           else{
+            hmap2.put(tc , sc);
+           }  
+        }
 
-       if(n != m){
-        return false ;
-       }
+        return true ;
 
-       for(int i = 0  ; i< n ; i++){
-
-        char sc = s.charAt(i);
-        char tc = t.charAt(i);
-        //   s-> t 
-         if(hmap1.containsKey(s.charAt(i))){
-            if(hmap1.get(s.charAt(i)) != tc  ){
-                return false;
-            }
-         }
-         else{
-            hmap1.put(sc ,tc);
-         }
-
-         //t->s 
-         if(hmap2.containsKey(t.charAt(i))){
-            if(hmap2.get(t.charAt(i)) != sc  ){
-                return false;
-            }
-         }
-         else{
-            hmap2.put(tc,sc);
-         }
-       }
-
-return true;
     }
 }
