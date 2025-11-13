13985df330d886cb0d0cb595155a960d6c19e381
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-13 15:00:06 +0530

    Time: 379 ms (4.81%), Space: 63.2 MB (5.72%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,33 @@
 class Solution {
-    public void swap(int[] arr ,int left , int right){
-            while(left < right){
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
-        }
-    public void rotate(int[] arr, int k) {
-        int n = arr.length;        
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
         k = k%n;
-        swap(arr , 0 , n-k-1);
-        swap(arr , n-k, n-1);
-        swap(arr , 0 , n-1);
+        int arr[] =  new int[k];
+        int i = 0 ;
+        int t = k ;
+        while(k-- > 0){
+            arr[i] = nums[n-i-1];
+            System.out.println(arr[i] + "  " + nums[n-i-1]);
+            i++;
+        }
+        k= t;
+        int j = n - k - 1 ;
+        i= 0 ;
 
-
-        
+        for(int a = 0 ; a <n ; a++){
+            System.out.println(nums[a]);
+        }
+        while(j != -1 ){
+             
+            nums[n-i-1] = nums[j] ;
+            System.out.println(nums[j] + "  " + nums[n-i-1]);
+            j--;i++;
+        }
+        i = 0 ;
+        while(i < k){
+            nums[i] = arr[k - i - 1 ];
+            i++;
+        }
     }
 }
