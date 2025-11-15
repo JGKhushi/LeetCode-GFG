3d04508fd9b0d5b80cd5a37b567f5c39f38aad05
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-15 19:06:14 +0530

    Time: 0 ms (100%), Space: 43 MB (6.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,34 @@
 class Solution {
+
+    public int fn(int nums[] , int n , int[] dp ,  int i   ){
+       if(i >= n){
+        return 0 ;
+       }
+     
+       if(dp[i] != -1){
+         return dp[i] ;
+       }
+
+       int npick =  fn(nums , n , dp , i+1);
+       int pick =    nums[i] + fn(nums , n , dp , i+2 );
+
+
+        dp[i] =  Math.max(npick , pick);
+
+        return dp[i];
+    }
+
     public int rob(int[] nums) {
-        int n = nums.length;
-        int prev1 = nums[0];
-        int prev2 = 0 ;
-        for(int i = 1 ; i< n ; i++){
-            int pick = nums[i];
-            if(i > 1)pick += prev2 ;
-            int notp = prev1;
+       int n = nums.length;
+       if(n==1)return nums[0];
 
-            int curr = Math.max(pick , notp);
-            prev2 = prev1;
-            prev1 = curr;
+       int dp[] = new int[n+1] ;
+        Arrays.fill(dp , -1);
 
-        }
-        return prev1;
+        int ans =  fn(nums ,  n  ,dp , 0 ) ;
+
+        return ans;
+     
+
     }
 }
