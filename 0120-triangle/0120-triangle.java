c7dd0978d74030bb8100cc717b94f6de13c4334a
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-18 14:43:38 +0530

    Time: 12 ms (6.35%), Space: 46.4 MB (6.21%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -3,16 +3,16 @@ class Solution {
         int n =  triangle.size() ;
         List<List<Integer>> dp = new ArrayList<>();
         dp.add(triangle.get(n-1));
+            int idx = 0 ;
         for(int i = n-2 ; i >= 0   ; i-- ){
-           
             List<Integer> temp = new ArrayList<>();
             for(int j =0 ; j<triangle.get(i).size()  ; j++){
-                 int ii =  triangle.get(i).get(j) + dp.get(dp.size() - 1).get(j);
-                 int ip1 = triangle.get(i).get(j) + dp.get(dp.size() - 1).get(j+1);
+                 int ii =  triangle.get(i).get(j) + dp.get(idx).get(j);
+                 int ip1 = triangle.get(i).get(j) + dp.get(idx).get(j+1);
                 temp.add(Math.min(ii , ip1)) ;
             }
             dp.add(temp);
-           
+            idx++;
         }
         return dp.get(n-1).get(0);
     }
