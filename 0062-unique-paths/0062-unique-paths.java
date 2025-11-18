2e7e957ac53346febd9cb95d48a142e2caa51083
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-18 10:20:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (17.66%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int solve(int m , int n ,int[][] dp){
+        if(m == 0 && n== 0){
+            return 1 ;
+        }
+        if(m < 0 || n<0){
+            return 0 ;
+        }
+        if(dp[m][n] != -1 ){
+            return dp[m][n];
+        }
+        int left = 0 ;
+        int up = 0 ;
+        if(n> 0){
+            left =  solve(m , n-1 , dp);
+        }
+        if(m > 0){
+            up = solve(m-1 , n , dp );
+        }
+
+        dp[m][n] =  left + up;
+
+        return dp[m][n] ;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n] ;
+        for(int i = 0 ; i <  m ; i++){
+        Arrays.fill(dp[i] , -  1);
+        }
+        int ans = solve(m-1 , n-1 , dp );
+        return ans ;
+    }
+}
