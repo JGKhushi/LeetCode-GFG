d881549e844a9789a6801e76c7f7a6ab338dddd9
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-19 14:02:34 +0530

    Time: 12 ms (7.96%), Space: 46.8 MB (7.36%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findFinalValue(int[] arr, int original) {
+        Arrays.sort(arr);
+        int n  = arr.length;
+        HashMap<Integer ,Integer> hmap = new HashMap<>() ;
+
+        for(int i = 0 ; i < n ; i ++){
+            hmap.put(arr[i] , hmap.getOrDefault(arr[i] , 0) + 1);
+        }
+       
+    
+        while(hmap.containsKey(original)){
+            original *= 2; 
+        }
+
+
+
+return original ;
+         
+    }
+}
