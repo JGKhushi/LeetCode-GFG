53ab2d6dce9f0149ee93def37f82c18117fc1162
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-29 15:04:31 +0530

    Time: 0 ms (100%), Space: 42.1 MB (27.83%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,32 +1,33 @@
 class Solution {
-
-    public int uniquePaths(int m, int n) {
-        int[] dp = new int[n] ;
-       
-        dp[0] = 1 ; 
-       
-        for(int i = 0 ; i <  m ; i++){
-            int temp[] = new int[n] ;
-            for(int j = 0 ; j< n ; j++){               
-                int right = 0 ;
-                int down = 0 ;
-                if(j>0){
-                right += temp[j-1] ; 
-                }
-                
-                down += dp[j];
-                
-
-          
-                temp[j] = right + down ;
-            
-           
-            
-            }
-
-            dp = temp;
-
+    
+    public int fn(int m , int n , int[][] dp , int i , int j  ){
+        if(i == m || j == n ){
+            return 0 ;
         }
-        return  dp[n-1] ;
+        if(i == m-1 && j == n-1 ){
+            return 1 ;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        //right
+        int right =  fn(m , n , dp , i , j+1 );
+        //down
+        int down =  fn(m , n , dp , i+1 , j );
+
+        return dp[i][j] = right + down ;
+         
+    }
+    public int uniquePaths(int m, int n) {
+        int count = 0 ;
+        int dp[][] = new int[m][n] ;
+        for(int i = 0 ;i<m;i++){
+            Arrays.fill(dp[i] , -1);
+        }
+       int  ans = fn(m , n , dp ,  0 , 0) ;
+
+        return ans ;
     }
 }
