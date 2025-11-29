1c7366a1547fc771a47dbf026171d4b6166f6a21
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-29 10:14:54 +0530

    Time: 0 ms (100%), Space: 41 MB (88.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,44 +1,18 @@
 class Solution {
-
-    public int fn(int[] nums , int[] dp , int i   ){
-        int n = nums.length;
-        if(i >= n ){
-            return 0 ;
-        }
-
-        if(dp[i] != -1){
-            return dp[i] ;
-        }
-
-        int pick = nums[i] + fn(nums , dp , i + 2);
-
-        int npick =  fn(nums , dp , i+1);
-
-        dp[i] = Math.max(pick , npick) ;
-
-        return dp[i] ; 
-    }
-
     public int rob(int[] nums) {
-        int n = nums.length ;
-        int dp[] = new int[n] ;
+        int n = nums.length;
+        int prev1 = nums[0];
+        int prev2 = 0 ;
+        for(int i = 1 ; i< n ; i++){
+            int pick = nums[i];
+            if(i > 1)pick += prev2 ;
+            int notp = prev1;
 
-        Arrays.fill(dp , -1);
-        // int ans = fn(nums , dp , 0 ) ;
+            int curr = Math.max(pick , notp);
+            prev2 = prev1;
+            prev1 = curr;
 
-        if(n==1){
-            return nums[0];
         }
-        if(n==2){
-            return Math.max(nums[0] , nums[1]);
-        }
-        dp[n-1]  = nums[n-1];
-        dp[n-2] = Math.max(nums[n-1] , nums[n-2]); 
-        for(int i = n-3 ; i>= 0 ; i--){
-            int pick =  nums[i] + dp[i+2];
-            int npick = dp[i+1] ; 
-            dp[i] = Math.max(pick , npick);
-        }
-        return dp[0] ;
+        return prev1;
     }
 }
