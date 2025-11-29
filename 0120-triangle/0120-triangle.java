41b6372e422d7e3697c942057988f4089798530b
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-29 21:56:22 +0530

    Time: 1 ms (99.81%), Space: 45.6 MB (45.86%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,20 +1,37 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n =  triangle.size() ;
-        List<List<Integer>> dp = new ArrayList<>();
-        dp.add(triangle.get(n-1));
-            int idx = 0 ;
-        for(int i = n-2 ; i >= 0   ; i-- ){
-            List<Integer> temp = new ArrayList<>();
-            for(int j =0 ; j<triangle.get(i).size()  ; j++){
-                 int ii =  triangle.get(i).get(j) + dp.get(idx).get(j);
-                 int ip1 = triangle.get(i).get(j) + dp.get(idx).get(j+1);
-                temp.add(Math.min(ii , ip1)) ;
-            }
-            dp.add(temp);
-            idx++;
+
+    public int fn(List<List<Integer>> triangle, int[][] dp, int i, int j) {
+        int n = triangle.size();
+
+        // base case: last row
+        if (i == n - 1) {
+            return triangle.get(i).get(j);
         }
-        return dp.get(n-1).get(0);
+
+        // memo check
+        if (dp[i][j] != Integer.MAX_VALUE) {
+            return dp[i][j];
+        }
+
+        int down = fn(triangle, dp, i + 1, j);         // move to same index
+        int diag = fn(triangle, dp, i + 1, j + 1);     // move to next index
+
+        // store answer
+        dp[i][j] = triangle.get(i).get(j) + Math.min(down, diag);
+
+        return dp[i][j];
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+
+        int[][] dp = new int[n][n];
+
+        // initialize dp with sentinel (meaning "not computed")
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
+        return fn(triangle, dp, 0, 0);
     }
 }
-
