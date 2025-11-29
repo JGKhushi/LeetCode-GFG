dded675747dd8823f4df2b739eb5cfea1581c090
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-29 09:55:41 +0530

    Time: 0 ms (100%), Space: 42.5 MB (39.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -24,8 +24,21 @@ class Solution {
         int dp[] = new int[n] ;
 
         Arrays.fill(dp , -1);
-        int ans = fn(nums , dp , 0 ) ;
+        // int ans = fn(nums , dp , 0 ) ;
 
-        return ans;
+        if(n==1){
+            return nums[0];
+        }
+        if(n==2){
+            return Math.max(nums[0] , nums[1]);
+        }
+        dp[n-1]  = nums[n-1];
+        dp[n-2] = Math.max(nums[n-1] , nums[n-2]); 
+        for(int i = n-3 ; i>= 0 ; i--){
+            int pick =  nums[i] + dp[i+2];
+            int npick = dp[i+1] ; 
+            dp[i] = Math.max(pick , npick);
+        }
+        return dp[0] ;
     }
 }
