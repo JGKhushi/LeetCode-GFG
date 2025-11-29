a92e78e3986f8e7080bd8ec7cf13c326d3a428ff
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-11-29 15:21:56 +0530

    Time: 0 ms (100%), Space: 41.8 MB (44.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,29 @@
 class Solution {
-    
-    public int fn(int m , int n , int[][] dp , int i , int j  ){
-        if(i == m || j == n ){
-            return 0 ;
-        }
-        if(i == m-1 && j == n-1 ){
-            return 1 ;
-        }
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        //right
-        int right =  fn(m , n , dp , i , j+1 );
-        //down
-        int down =  fn(m , n , dp , i+1 , j );
-
-        return dp[i][j] = right + down ;
-         
-    }
     public int uniquePaths(int m, int n) {
-        int count = 0 ;
-        int dp[][] = new int[m][n] ;
-        for(int i = 0 ;i<m;i++){
-            Arrays.fill(dp[i] , -1);
-        }
-       int  ans = fn(m , n , dp ,  0 , 0) ;
 
-        return ans ;
+        int[][] dp = new int[m][n];
+
+        // Base: bottom-right is 1
+        dp[m-1][n-1] = 1;
+
+        // Fill last row (right direction only)
+        for (int j = n-2; j >= 0; j--) {
+            dp[m-1][j] = 1;
+        }
+
+        // Fill last column (down direction only)
+        for (int i = m-2; i >= 0; i--) {
+            dp[i][n-1] = 1;
+        }
+
+        // Fill remaining cells bottom-up
+        for (int i = m-2; i >= 0; i--) {
+            for (int j = n-2; j >= 0; j--) {
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+
+        return dp[0][0];
     }
 }
